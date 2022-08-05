a1c3004a6e8cc792f5ab2c9a00e1d60a4716ba49
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-05 21:22:35 +0530

    Update RegisteredUser.java
    created method cancel Ticket

--- MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/RegisteredUser.java
+++ MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/RegisteredUser.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 
 public class RegisteredUser extends User{
 	
-	private ArrayList<Ticket> bookingHistory;
+	public ArrayList<Ticket> bookingHistory;
 
 	public RegisteredUser(int id, String name) {
 		super(id, name);
 		this.bookingHistory = new ArrayList<>();
 	}
+	
+	public void cancelTicket(Ticket ticket){
+
+    }
 
 }
