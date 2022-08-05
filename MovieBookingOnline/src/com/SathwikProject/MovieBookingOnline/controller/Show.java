ba87d7d34d1b49af25844e883e937ffcf43a97fb
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-05 21:24:17 +0530

    Update Show.java
    added toString() override method and updated id so it will be in sequence

--- MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/Show.java
+++ MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/Show.java
@@ -4,20 +4,26 @@ import java.util.Date;
 
 public class Show {
 
+	private static int idCounter = 0;
 	private int id;
 	private Date showTime;
 	private int availableSeats;
-	
 	private Movie movie;
 	private Theater theater;
+	
 	public Show(int id, Date showTime, int availableSeats, Movie movie, Theater theater) {
-		super();
-		this.id = id;
+		idCounter += 1;
+		this.id = idCounter;
 		this.showTime = showTime;
 		this.movie = movie;
 		this.theater = theater;
 		this.availableSeats = theater.getCapacity();
+		theater.getShows().add(this);
 	}
+	public void updateShow(){
+		
+    }
+	
 	public int getId() {
 		return id;
 	}
@@ -49,6 +55,32 @@ public class Show {
 		this.theater = theater;
 	}
 	
-	
+	public synchronized Ticket bookTicket(RegisteredUser user, int seats){
+        if(availableSeats >= seats && seats >0){
+            Ticket ticket = new Ticket();
+            availableSeats -= seats;
+            ticket.setOwner(user.getName());
+            ticket.setBookedShow(this);
+            ticket.setBookingTime(new Date());
+            ticket.setNumberOfSeats(seats);
+            System.out.println("Successfully booked");
+            user.bookingHistory.add(ticket);
+            return ticket;
+        }
+        else{
+            System.out.println("Seats not Available");
+            return null;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Show{" +
+                "id=" + id +
+                ", showTime=" + showTime +
+                ", movie=" + movie.getName() +
+                ", theater=" + theater.getName() +
+                ", availableSeats=" + availableSeats +
+                '}';
+    }
 	
 }
