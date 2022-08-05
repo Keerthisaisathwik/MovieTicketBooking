702db8337c8745432a4996f50187265a6ffd5211
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-05 21:19:53 +0530

    Update MainClass.java
    nothing important

--- MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/MainClass.java
+++ MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/MainClass.java
@@ -2,6 +2,8 @@ package com.SathwikProject.MovieBookingOnline.controller;
 
 import java.util.ArrayList;
 
+//I need to Update this class I will do it later 05-08-2022.
+//create Ticket Booking Thread.
 public class MainClass {
 
 	private ArrayList<Theater> theater;
