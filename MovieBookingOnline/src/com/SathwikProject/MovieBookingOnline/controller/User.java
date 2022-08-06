617c8c1c21fad2ee05c3fdb7019b436a30e13546
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-06 21:14:02 +0530

    Update User.java
    added Accessors.
    id Counter key word for customer id's.

--- MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/User.java
+++ MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/User.java
@@ -2,11 +2,17 @@ package com.SathwikProject.MovieBookingOnline.controller;
 
 public abstract class User {
 
+	private static int idCounter = 0;
 	private int id;
 	private String name;
 	public User(int id, String name) {
-		this.id = id;
+		idCounter += 1;
+		this.id = idCounter;
 		this.name = name;
 	}
+	
+	public String getName() {
+        return name;
+    }
 	
 }
