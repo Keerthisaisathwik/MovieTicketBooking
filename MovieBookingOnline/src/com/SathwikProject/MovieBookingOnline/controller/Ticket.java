c1e6051e4648fe316e8c78b9be4129acc72681a2
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-06 21:12:28 +0530

    Update Ticket.java
    removed controller.(not used anyway)
    added toString() method to print perfectly.(useful until project completion)
    id Counter is added for continues key values(1 to n).

--- MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/Ticket.java
+++ MovieBookingOnline/src/com/SathwikProject/MovieBookingOnline/controller/Ticket.java
@@ -2,53 +2,85 @@ package com.SathwikProject.MovieBookingOnline.controller;
 
 import java.util.Date;
 
-public class Ticket {
+public class Show {
 
+	private static int idCounter = 0;
 	private int id;
-	private Date bookingTime;
-	private String theater;
-	private int noOfSeats;
+	private Date showTime;
+	private int availableSeats;
+	private Movie movie;
+	private Theater theater;
 	
-	private Show bookedShow;
-	
-	public Ticket(int id, Date bookingTime, String theater, int noOfSeats, Show bookedShow) {
-		super();
-		this.id = id;
-		this.bookingTime = bookingTime;
+	public Show(int id, Date showTime, int availableSeats, Movie movie, Theater theater) {
+		idCounter += 1;
+		this.id = idCounter;
+		this.showTime = showTime;
+		this.movie = movie;
 		this.theater = theater;
-		this.noOfSeats = noOfSeats;
-		this.bookedShow = bookedShow;
+		this.availableSeats = theater.getCapacity();
+		theater.getShows().add(this);
 	}
+	public void updateShow(){
+		
+    }
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public Date getBookingTime() {
-		return bookingTime;
+	public Date getShowTime() {
+		return showTime;
 	}
-	public void setBookingTime(Date bookingTime) {
-		this.bookingTime = bookingTime;
+	public void setShowTime(Date showTime) {
+		this.showTime = showTime;
 	}
-	public String getTheater() {
+	public int getAvailableSeats() {
+		return availableSeats;
+	}
+	public void setAvailableSeats(int availableSeats) {
+		this.availableSeats = availableSeats;
+	}
+	public Movie getMovie() {
+		return movie;
+	}
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+	public Theater getTheater() {
 		return theater;
 	}
-	public void setTheater(String theater) {
+	public void setTheater(Theater theater) {
 		this.theater = theater;
 	}
-	public int getNoOfSeats() {
-		return noOfSeats;
-	}
-	public void setNoOfSeats(int noOfSeats) {
-		this.noOfSeats = noOfSeats;
-	}
-	public Show getBookedShow() {
-		return bookedShow;
-	}
-	public void setBookedShow(Show bookedShow) {
-		this.bookedShow = bookedShow;
-	}
 	
+	public synchronized Ticket bookTicket(RegisteredUser user, int seats){
+        if(availableSeats >= seats && seats >0){
+            Ticket ticket = new Ticket();
+            availableSeats -= seats;
+            ticket.setOwner(user.getName());
+            ticket.setBookedShow(this);
+            ticket.setBookingTime(new Date());
+            ticket.setNumberOfSeats(seats);
+            System.out.println("Successfully booked");
+            user.bookingHistory.add(ticket);
+            return ticket;
+        }
+        else{
+            System.out.println("Seats not Available");
+            return null;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Show{" +
+                "id=" + id +
+                ", showTime=" + showTime +
+                ", movie=" + movie.getName() +
+                ", theater=" + theater.getName() +
+                ", availableSeats=" + availableSeats +
+                '}';
+    }
 	
 }
